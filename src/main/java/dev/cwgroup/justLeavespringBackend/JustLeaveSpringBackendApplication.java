7102449246d071ef8ed0c341810054466e7fc5f6
Imran Rasheed <imran.2001r@gmail.com>
2022-06-08 23:28:37 +0530

    Initial commit

---
+++ src/main/java/dev/cwgroup/justLeavespringBackend/JustLeaveSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.cwgroup.justLeavespringBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustLeaveSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustLeaveSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cwgroup/justLeavespringBackend/JustLeaveSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cwgroup.justLeavespringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustLeaveSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
